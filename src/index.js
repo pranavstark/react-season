8efcaed93d52f6dc97d3e338f314a980225a707e
pranav <pranavstark007@gmail.com>
2019-01-10 00:15:06 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <p>Hi There!</p>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App/>, document.querySelector("#root"));
