9b4a27bde7bdb98a6866e8416c27327ad22dd907
pranav <pranavstark007@gmail.com>
2019-01-10 01:02:06 +0530

    created a class based components with state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return (
-        <div>
-            <p>Hi There!</p>
-        </div>
-    )
+//functional component
+// const App = () => {
+//     return (
+//         <div>
+//             <p>Hi There!</p>
+//         </div>
+//     )
+// }
+
+//class based component
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state  = {lat: null};
+
+    
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                console.log(position)
+                this.setState({lat: position.coords.latitude});
+            },
+            err => console.log(err)
+        );
 }
 
+    render(){
+        return <div> Latitude: {this.state.lat}</div>;
+    }
+}
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(<App/>, document.querySelector('#root'));
